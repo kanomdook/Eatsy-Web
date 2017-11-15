f8929dc910147db84c5cb37c9751edbd16813d6e
kanomdook <kanung.2535@gmail.com>
2017-11-15 17:08:09 +0700

    add get shop from googleMap

--- src/app/manage-shop/manage-shop.component.ts
+++ src/app/manage-shop/manage-shop.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Overlay } from 'ngx-modialog';
 import { Modal } from 'ngx-modialog/plugins/bootstrap';
+declare var google;
 
 @Component({
   selector: 'app-manage-shop',
@@ -8,30 +9,42 @@ import { Modal } from 'ngx-modialog/plugins/bootstrap';
   styleUrls: ['./manage-shop.component.css']
 })
 export class ManageShopComponent implements OnInit {
+  @ViewChild('map') mapElement: ElementRef;
 
+  private shopList: Array<any> = [];
   constructor(public modal: Modal) { }
 
   ngOnInit() {
-  }
+    let latLng = {
+      lat: 13.7466532,
+      lng: 100.5347222
+    };
 
-  onClick() {
-    const dialogRef = this.modal.alert()
-      .size('lg')
-      .showClose(true)
-      .title('เพิ่มข้อมูลร้านค้า')
-      .body(``)
-      .open();
+    let map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 15,
+      center: latLng
+    });
 
-    dialogRef
-      .then(dialogRef => {
-        dialogRef.result.then(result => {
-          alert(result);
+    let request = {
+      location: latLng,
+      radius: '50000',
+      types: ["cafe", "restaurant", "food", "store"],
+      keyword: 'coffee'
+    };
+
+    let service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, (results, status) => {
+      if (status == 'OK') {
+
+        results.forEach(element => {
+          element.img = element.photos ? element.photos[0].getUrl({ 'maxWidth': 318, 'maxHeight': 180 }) : 'http://www.freeiconspng.com/uploads/no-image-icon-15.png';
         });
-      });
-  }
 
-  apple(dook) {
-    console.log(dook);
+        this.shopList = results;
+        console.log(this.shopList);
+      }
+    });
+
   }
 
 }
