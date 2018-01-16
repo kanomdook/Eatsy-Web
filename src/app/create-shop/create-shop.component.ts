badbc070ce55999ed62891f19e47dc98934e61c6
kanomdook <kanung.2535@gmail.com>
2018-01-16 11:55:10 +0700

    Revert "active menu"
    This reverts commit 68a1325525130ad868f981361420fec5242adeab.

--- src/app/create-shop/create-shop.component.ts
+++ src/app/create-shop/create-shop.component.ts
@@ -77,13 +77,12 @@ export class CreateShopComponent implements OnInit {
   private prdName: string = '';
   private productModel: any = {};
   private productImgPreSaves: Array<any> = [];
-  private activeMenu: Array<any> = [];
   private isDelete: boolean = false;
   promoteIsEdit: boolean = false;
   updateOrEditCateImg: any;
   limitPrdImg = 3;
   constructor(private server: ServerConfig, private router: Router, private shopService: ShopService, private pubsub: PubSubService
-
+    // private modalService: BsModalService
   ) {
     this.galleryOptions = [
       {
@@ -114,7 +113,6 @@ export class CreateShopComponent implements OnInit {
       }
     ];
 
-    this.setActiveMenu(0);
   }
 
   selectProductImg() {
@@ -913,19 +911,8 @@ export class CreateShopComponent implements OnInit {
       }
     }
   }
-
-  setActiveMenu(tab_id) {
-    for (let i = 0; i < 4; i++) {
-      if (i === tab_id) {
-        this.activeMenu[i] = 'btn btn-fb-checked';
-      } else {
-        this.activeMenu[i] = 'btn';
-      }
-    }
-  }
-
   selectTab(tab) {
-    this.setActiveMenu(tab);
+
     switch (tab) {
       case 0: {
         this.selectTabs = 0;
